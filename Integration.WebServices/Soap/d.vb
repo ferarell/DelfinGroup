b0cd31cb37d6e575cc448c069fe0a8524d825ea4
lucia <lucia@DESKTOP-CM6U3BD>
2020-11-17 23:39:31 -0500

    17112020 --> Agregar Clases para Busqueda Documentos

---
+++ Integration.WebServices/Soap/d.vb
@@ -0,0 +1,12 @@
+﻿Public Class d
+    Dim _results As List(Of results)
+
+    Public Property results As List(Of results)
+        Get
+            Return _results
+        End Get
+        Set(value As List(Of results))
+            _results = value
+        End Set
+    End Property
+End Class
---
+++ Integration.WebServices/Soap/results.vb
@@ -0,0 +1,32 @@
+﻿Public Class results
+    Dim _DocEntry As String
+    Dim _FolioNum As String
+    Dim _FolioPref As String
+
+    Public Property DocEntry As String
+        Get
+            Return _DocEntry
+        End Get
+        Set(value As String)
+            _DocEntry = value
+        End Set
+    End Property
+
+    Public Property FolioNum As String
+        Get
+            Return _FolioNum
+        End Get
+        Set(value As String)
+            _FolioNum = value
+        End Set
+    End Property
+
+    Public Property FolioPref As String
+        Get
+            Return _FolioPref
+        End Get
+        Set(value As String)
+            _FolioPref = value
+        End Set
+    End Property
+End Class
