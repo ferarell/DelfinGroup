2ac64d583592d14a61f2affea5fac7a496af6573
Fernando Arellano <fernando.arellano@totalit.com.pe>
2021-01-07 10:54:17 -0500

    update 070120211055

--- Application.WebServices/DelfinService.svc.vb
+++ Application.WebServices/DelfinService.svc.vb
@@ -1192,8 +1192,8 @@ Public Class DelfinService
 
 
     Public Function PreFacturar(PDOC_Codigo As Integer, AUDI_UsrCrea As String) As Boolean Implements IDelfinService.PreFacturar
-        'Dim DocsVta_BL As New DocsVta_BL
-        'Return DocsVta_BL.PreFacturar(PDOC_Codigo, AUDI_UsrCrea)
+        Dim DocsVta_BL As New DocsVta_BL
+        Return DocsVta_BL.PreFacturar(PDOC_Codigo, AUDI_UsrCrea)
     End Function
 
 
